594670053b19f2c86a81facb58d05af3032767dd
maben <maben_jm@aliyun.com>
2021-05-18 06:48:12 +0800

    多线程学习

---
+++ druid-monitor/src/test/java/com/maben/druid_monitor/TestCorrectPostureStep2.java
@@ -0,0 +1,51 @@
+package com.maben.druid_monitor;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static com.maben.druid_monitor.util.Sleeper.sleep;
+
+@Slf4j(topic = "m.TestCorrectPosture")
+public class TestCorrectPostureStep2 {
+    static final Object room = new Object();
+    static boolean hasCigarette = false;
+    static boolean hasTakeout = false;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (room) {
+                log.debug("有烟没？[{}]", hasCigarette);
+                if (!hasCigarette) {
+                    log.debug("没烟，先歇会！");
+                    try {
+                        room.wait(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                log.debug("有烟没？[{}]", hasCigarette);
+                if (hasCigarette) {
+                    log.debug("可以开始干活了");
+                }
+            }
+        }, "小南").start();
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                synchronized (room) {
+                    log.debug("可以开始干活了");
+                }
+            }, "其它人").start();
+        }
+
+        sleep(1);
+        new Thread(() -> {
+            synchronized (room) {
+                hasCigarette = true;
+                log.debug("烟到了噢！");
+                //notify也要配合synchronized使用
+                room.notify();
+            }
+        }, "送烟的").start();
+    }
+
+}
